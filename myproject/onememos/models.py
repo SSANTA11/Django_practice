6a3e498e5184dcca274e52bb8325d4afbf570e01
SSANTA11 <ssanta011205@skuniv.ac.kr>
2024-10-06 23:37:59 +0900

    10월 6일 장고 model의 이해

--- myproject/onememos/models.py
+++ myproject/onememos/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+# 모델의 기능은 테이블(=DB)생성
+# 앱을 만드는데 있어서 DB는 반드시 필요하다. 이를 다룰 방법을 설계하는 것이 데이터 모델이다
+# 왜 이름이 모델일까?
+    # 뭔가를 대표하고 표본으로 고려되는 단어, 모델
